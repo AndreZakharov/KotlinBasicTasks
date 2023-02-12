a67bc6dfe03f0b5dd57f6f3b6869509441f6074f
AndreZakharov <122970684+AndreZakharov@users.noreply.github.com>
2023-02-12 21:36:46 +0300

    4 module 19 task

--- src/main/kotlin/module4/Aircraft.kt
+++ src/main/kotlin/module4/Aircraft.kt
@@ -1,6 +1,6 @@
 package module4
 
-class Aircraft (_numberAirplane:Int,_rangeOfFlight:Double,_tankCapacity:Double){
+open class Aircraft (_numberAirplane:Int,_rangeOfFlight:Double,_tankCapacity:Double){
     var numberAirplane:Int
     var rangeOfFlight:Double
     var tankCapacity:Double
@@ -11,4 +11,8 @@ class Aircraft (_numberAirplane:Int,_rangeOfFlight:Double,_tankCapacity:Double){
         rangeOfFlight=_rangeOfFlight
         tankCapacity=_tankCapacity
     }
+}
+class Boeing747:Aircraft{
+var passengerCapacity:Int =400
+    constructor(_numberAirplane: Int,_rangeOfFlight: Double,_tankCapacity: Double) : super(_numberAirplane,_rangeOfFlight,_tankCapacity)
 }
