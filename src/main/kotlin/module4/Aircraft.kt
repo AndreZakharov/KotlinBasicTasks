c5ee28451b50831cc5a64263ab7caa776a3aaaf8
AndreZakharov <122970684+AndreZakharov@users.noreply.github.com>
2023-02-12 20:59:15 +0300

    4 module 18 task

--- src/main/kotlin/module4/Aircraft.kt
+++ src/main/kotlin/module4/Aircraft.kt
@@ -1,9 +1,14 @@
 package module4
 
-class Aircraft {
-    var numberAirplane:Int = 777
-    var rangeOfFlight:Double = 10000.0
-    var tankCapacity:Double = 180000.0
+class Aircraft (_numberAirplane:Int,_rangeOfFlight:Double,_tankCapacity:Double){
+    var numberAirplane:Int
+    var rangeOfFlight:Double
+    var tankCapacity:Double
     var fuelConsumptionFor100km:Double = 1800.0
         get() = tankCapacity/rangeOfFlight*100
+    init {
+        numberAirplane=_numberAirplane
+        rangeOfFlight=_rangeOfFlight
+        tankCapacity=_tankCapacity
+    }
 }
