1d04f344f818c40f96a238c4c0aa0bc6a45b3e5f
AndreZakharov <122970684+AndreZakharov@users.noreply.github.com>
2023-02-12 23:51:02 +0300

    4 module 22 task
    add and override abstract fun

--- src/main/kotlin/module4/Aircraft.kt
+++ src/main/kotlin/module4/Aircraft.kt
@@ -1,18 +1,25 @@
 package module4
 
 abstract class Aircraft (_numberAirplane:Int,_rangeOfFlight:Double,_tankCapacity:Double){
-    var numberAirplane:Int
-    var rangeOfFlight:Double
-    var tankCapacity:Double
-    var fuelConsumptionFor100km:Double = 1800.0
+    protected var numberAirplane:Int
+    protected var rangeOfFlight:Double
+    protected var tankCapacity:Double
+    protected var fuelConsumptionFor100km:Double = 1800.0
         get() = tankCapacity/rangeOfFlight*100
     init {
         numberAirplane=_numberAirplane
         rangeOfFlight=_rangeOfFlight
         tankCapacity=_tankCapacity
     }
+    abstract fun infoAboutAirplane()
 }
 class Boeing747:IPassenger,Aircraft{
     override var passengerCapacity: Int = 400
     constructor(_numberAirplane: Int,_rangeOfFlight: Double,_tankCapacity: Double) : super(_numberAirplane,_rangeOfFlight,_tankCapacity)
+
+    override fun infoAboutAirplane() {
+       println("Airplane №: $numberAirplane \nrange Of Flight: $rangeOfFlight \ntank Capacity: $tankCapacity \n" +
+               "fuel Consumption For 100 km: $fuelConsumptionFor100km \npassenger Capacity: $passengerCapacity")
+    }
+
 }
